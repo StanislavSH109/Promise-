1097768cbe727c59caaba3c326d34b009ccbd780
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-10-22 23:43:08 +0300

    add gif and create styles in header, main, reboot

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const imageStockUrl = 'https://t3.ftcdn.net/jpg/02/73/16/24/240_F_273162497_ShAAB1TH0vhM4UUWbhBuao8jtGBDubwD.jpg';
-const mainElement = document.querySelector('.main-section');
-const titleElement = document.querySelector('.header__title-text');
+const arrayURL = [
+    'gif/29078-374013301_tiny.gif',
+    'gif/32767-394004551_tiny.gif',
+    'gif/37946-415263561_tiny.gif'
+]
 
-const divImage = document.createElement('div');
-const imgElement = document.createElement('img');
-imgElement.setAttribute('src', imageStockUrl);
-divImage.classList.add('main-section__image');
-divImage.append(imgElement)
-
-titleElement.addEventListener('click', function (e) {
-    divImage.classList.toggle('visible');
+document.addEventListener('DOMContentLoaded', function (e) {
+    setTimeout(() => {
+       console.log('Hello!');;
+    }, 3000)
 });
 
-mainElement.append(divImage);
+// const imageStockUrl = 'https://t3.ftcdn.net/jpg/02/73/16/24/240_F_273162497_ShAAB1TH0vhM4UUWbhBuao8jtGBDubwD.jpg';
+// const mainElement = document.querySelector('.main-section');
+// const titleElement = document.querySelector('.header__title-text');
+
+// const divImage = document.createElement('div');
+// const imgElement = document.createElement('img');
+// imgElement.setAttribute('src', imageStockUrl);
+// divImage.classList.add('main-section__image');
+// divImage.append(imgElement)
+
+// titleElement.addEventListener('click', function (e) {
+//     divImage.classList.toggle('main-section__image--visible');
+// });
+
+// mainElement.append(divImage);
