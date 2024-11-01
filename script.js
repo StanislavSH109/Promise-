ab7f2e965c3d0c05ab541000c69b299b080b08b5
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-01 06:29:36 +0300

    +++

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-const myPromise = new Promise((resolve, reject) => {
-    const x = 101;
+const arrayURL = [
+    'gif/29078-374013301_tiny.gif',
+    'gif/32767-394004551_tiny.gif',
+    'gif/37946-415263561_tiny.gif'
+]
+
+function createCard(firstArray) {
+    const mainElement = document.querySelector('.main-section');
+    const divElement = document.createElement('div');
+    divElement.classList.add('card');
     
-    if (x === 10) {
-        resolve('This is Work!');
-    } else {
-        reject('Error');
-    }
-})
-
-console.log(myPromise);
-
-// const arrayURL = [
-//     'gif/29078-374013301_tiny.gif',
-//     'gif/32767-394004551_tiny.gif',
-//     'gif/37946-415263561_tiny.gif'
-// ]
-
-// function createCard(firstArray) {
-//     const mainElement = document.querySelector('.main-section');
-//     const divElement = document.createElement('div');
-//     divElement.classList.add('card');
-    
-//     arrayURL.forEach((item, index) => {
-//         const imageElement = document.createElement('img');
-//         imageElement.setAttribute('src', firstArray[index]);
-//         divElement.append(imageElement);
-//         mainElement.append(divElement);
+    arrayURL.forEach((item, index) => {
+        const imageElement = document.createElement('img');
+        imageElement.setAttribute('src', firstArray[index]);
+        divElement.append(imageElement);
+        mainElement.append(divElement);
         
-//     });
-// }
+    });
+}
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     setTimeout(() => {
-//         createCard(arrayURL);
-//     }, 3000);
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        createCard(arrayURL);
+    }, 3000);
 
-//     setTimeout(() =>)
-// });
+    setTimeout(() =>)
+});
 
