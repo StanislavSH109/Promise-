7fba73434cb87cb2ce591437a332cb9b6d2adbbe
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-01 06:46:55 +0300

    +++

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-const arrayURL = [
+// const myPromise = new Promise((resolve, reject) => {
+//     const x = 10;
+//     if (x === 10) {
+//         resolve('Resolve');
+//     } else {
+//         reject('Error');
+//     }
+// })
+
+// console.log(myPromise);
+
+const arrayFirstRowURL = [
     'gif/29078-374013301_tiny.gif',
     'gif/32767-394004551_tiny.gif',
     'gif/37946-415263561_tiny.gif'
+]
+
+const arraySecondRowURL = [
+    ''
 ]
 
 function createCard(firstArray) {
     const mainElement = document.querySelector('.main-section');
     const divElement = document.createElement('div');
     divElement.classList.add('card');
-    
+
 
     const urlPromise = new Promise((resolve, reject) => {
         if (firstArray) {
-            resolve(arrayURL.forEach((item, index) => {
+            firstArray.forEach((item, index) => {
                 const imageElement = document.createElement('img');
                 imageElement.setAttribute('src', firstArray[index]);
                 divElement.append(imageElement);
-                mainElement.append(divElement);
-                
-            }));
+            });
+            mainElement.append(divElement);
+            resolve('Карточки успешно созданы')
         } else {
             reject('Ошибка массива URL');
         }
     })
-    console.log(urlPromise);
     return urlPromise;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        createCard(arrayURL);
+        createCard(arrayFirstRowURL)
+            .then((message) => console.log(message))
+            .catch((error) => console.log(error));
     }, 3000);
 
 });
