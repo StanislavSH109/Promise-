8c579ac8f17289ae9ca6a69782e314ddcca1bc83
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-04 10:36:16 +0300

    Load min size gif

--- js/random.js
+++ js/random.js
@@ -2,6 +2,6 @@ export function myRandom() {
     const min = 2000;
     const max = 5000;
     let randomTime = (Math.floor(Math.random() * (max - min) + min));
-    console.log(randomTime);
+
     return randomTime;
 }
